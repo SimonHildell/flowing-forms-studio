aceb8b163a08d8b6a4575e49b0b394c835e93583
Simon Hildell <nomis.hildell@gmail.com>
2026-01-13 10:25:35 +0100

    UI changes to add projects selection and about

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
+  </HashRouter>
+);
